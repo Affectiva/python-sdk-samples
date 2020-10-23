46a7ed09c8e9b2928331923f59628b540b60bdf3
Andy Dennie <andy.dennie@affectiva.com>
2020-10-23 13:42:24 -0400

    SDK-3828: Adapt to API changes for Object, Occupant, Body
    Update code to use getter methods instead of attributes.

--- occupant_listener.py
+++ occupant_listener.py
@@ -43,27 +43,27 @@ class OccupantListener(af.OccupantListener):
         self.occupants = occupants
         self.clear_all_dictionaries()
         for occid, occ in occupants.items():
-            bbox = occ.bounding_box
+            bbox = occ.get_bounding_box()
             self.bounding_box[occid] = [bbox.getTopLeft().x,
                                         bbox.getTopLeft().y,
                                         bbox.getBottomRight().x,
                                         bbox.getBottomRight().y]
-            self.regionId[occid] = occ.matched_seat.cabin_region.id
-            self.regionType[occid] = occ.matched_seat.cabin_region.type.name
+            self.regionId[occid] = occ.get_matched_seat().cabin_region.id
+            self.regionType[occid] = occ.get_matched_seat().cabin_region.type.name
             if self.regionId[occid] != -1:
-                self.region[occid] = occ.matched_seat.cabin_region.vertices
+                self.region[occid] = occ.get_matched_seat().cabin_region.vertices
             # TODO: research the bug where we are not able to access object directly and gives "munmap_chunk(): invalid pointer"
-            # body_occ = occ.body
+            # body_occ = occ.get_body()
             # if body_occ is not None:
-            #     self.bodyId[occid] = body_occ.body_id
-            #     b_pts = body_occ.body_points
+            #     self.bodyId[occid] = body_occ.get_body_id()
+            #     b_pts = body_occ.get_body_points()
             #     body_points = {}
             #     for b_pt, pt in b_pts.items():
             #         body_points[b_pt.name] = [int(pt.x), int(pt.y)]
             #     self.bodyPoints[occid] = body_points
             # else:
             self.bodyId[occid] = "nan"
-            self.confidence[occid] = occ.matched_seat.match_confidence
+            self.confidence[occid] = occ.get_matched_seat().match_confidence
         self.mutex.release()
 
     def get_callback_interval(self):
